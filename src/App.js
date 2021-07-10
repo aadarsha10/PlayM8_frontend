a60a6436a377a184bc90fa207b482572d721424a
Dejina Ghale <ghaledejina@gmail.com>
2021-07-10 10:34:16 +0545

    changes on css of registration form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-bootstrap/dist/react-bootstrap.min.js";
-import Registration from './Components/Registration';
+import Registration from './Authentication/Registration';
+import RegistrationPlayer from './Authentication/RegistrationPlayer';
 
 function App() {
   return (
     <div className="App">
       <Registration />
+      
     </div>
   );
 }
