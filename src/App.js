932f7268a099e56b0cd65b1940083424cc7f4f07
diptibaral <premaswoorupa@gmail.com>
2021-07-15 10:50:10 +0545

    js of app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
+import React from 'react';
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-bootstrap/dist/react-bootstrap.min.js";
-<<<<<<< HEAD
 import "./App.css";
 import Footer from "./Components/Footer";
+import NavBar from "./Components/NavBar";
 // import Registration from './Components/Registration';
+import Dashboard from "./UI/Dashboard";
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Organizer_login from './UI/Organizer_login';
-=======
+// import Dashboard_organizer from './UI/Dashboard_organizer';
 import Registration from './Authentication/Registration';
 import RegistrationPlayer from './Authentication/RegistrationPlayer';
->>>>>>> 99db1e1fbbb1eda687aaee7c20aa36743334d29b
 
 function App() {
   return (
+    <Router>
     <div className="App">
-<<<<<<< HEAD
-      {/* <Registration /> */}
-      < Organizer_login/>
+   <NavBar/>
+       {/* <Dashboard_organizer/> */}
+       <Route path = "/Organizer_login" component= {Organizer_login}/>
+      {/* < Organizer_login/> */}
       <Footer/>
-=======
-      <Registration />
-      
->>>>>>> 99db1e1fbbb1eda687aaee7c20aa36743334d29b
+ 
     </div>
+    </Router>
   );
 }
 
