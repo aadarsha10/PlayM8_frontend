3ab8e70320590fcb00b364d0229234fdb8b7bdf6
diptibaral <premaswoorupa@gmail.com>
2021-07-15 10:58:26 +0545

    Create Dashboard

--- src/UI/Dashboard_organizer.js
+++ src/UI/Dashboard_organizer.js
@@ -1,1 +1,16 @@
 // something here
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "react-bootstrap/dist/react-bootstrap.min.js";
+import "./App.css";
+
+
+function Dashboard_organizer() {
+  return (
+    <div >
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
+
+export default Dashboard_organizer;
