59ab11eb935ae9440788c4e4c7f6e6ad79270a8f
diptibaral <premaswoorupa@gmail.com>
2021-07-17 12:59:40 +0545

    changes in dashboard

--- src/UI/Landing_Main.js
+++ src/UI/Landing_Main.js
@@ -4,6 +4,8 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 import RegistrationPlayer from '../Authentication/RegistrationPlayer';
 import RegistrationOrganizer from '../Authentication/RegistrationOrganizer';
 import Login from './Organizer_login';
+import PlayerLogin from './Player_login';
+
 
 
 class Landing_Main extends Component {
@@ -15,14 +17,16 @@ class Landing_Main extends Component {
             // <Route path='/register' component={Register}></Route>
             // </Switch> */}
             <div>
-                {/* <Route path='/home' component={Home} /> */}
+                { <Route path='/home' component={} />}
                 <Route path='/organizer/register' exact component={RegistrationOrganizer} />
                 <Route path='/player/register' exact component={RegistrationPlayer} />
-                <Route path='/login' exact component={Login} />
+                <Route path='/organizer/login' exact component={Login} />
+                <Route path='/player/login' exact component={PlayerLogin} />
                 {/* <Route path='/UserLanding' component={UserLanding}/> */}
             </div>
         )
 
     }
+    
 }
 export default Landing_Main;
--- src/UI/Player_login.js
+++ src/UI/Player_login.js
@@ -1,34 +1,33 @@
 import React, { useState } from "react";
 import NavBar from "../Components/NavBar";
-import "../scss/Registration.scss";
+import "../scss/login.scss";
 import { Form } from "react-bootstrap";
 // import Alert from 'react-bootstrap/Alert'
 import axios from "axios";
 
-export default function Player_login() {
-  const [GameTitle, setGameTitle] = useState("");
-  const [Description, setDescription] = useState("");
+export default function PlayerLogin() {
+  const [Username, setUsername] = useState("");
+  const [Password, setPassword] = useState("");
   const [ShowAlert, setShowAlert] = useState(null);
   // const [validate, setValidate] = useState(false)
 
-  console.log("gameTitle", GameTitle);
+  console.log("Username", Username);
 
   const data = {
-    GameTitle: GameTitle,
-    Image: "Image.jpg",
-    Description: Description,
+    Username: Username,
+    Password: Password
   };
 
-  const addEvent = (e) => {
+  const loginPlayer = (e) => {
     e.preventDefault();
 
-    axios.post("http://localhost:80/addEvent", data).then((response) => {
+    axios.post("http://localhost:90/player/login", data).then((response) => {
       console.log("response", response);
 
       if (response.data.message === "Fields Must not be Empty") {
         setShowAlert(false);
       } else {
-        alert("Event Added Successfully");
+        alert("Logged in Successfully");
         setShowAlert(true);
         console.log("alert", ShowAlert);
       }
@@ -41,7 +40,7 @@ export default function Player_login() {
       {ShowAlert === false && (
         <div className="alert alert-danger">
           <h4 className="alert-heading">
-            Hello Oraganizer. Sorry to Say THAT !!
+            Hello Player. Sorry to Say THAT !!
           </h4>
           <p>
             You have failed to insert correct data. You must fill all the fields
@@ -57,58 +56,14 @@ export default function Player_login() {
           
           <div className="register">
            <form>
-             <h1>Register</h1>
-           <div class="row">
-                   <div class="form-group col-md-6">
-                            <Form.Label for="inputEmail4">FullName</Form.Label>
-                              <Form.Control
-                                type="text"  placeholder="Enter your Full name"
-                                 onChange={(event) => {
-                                 return setGameTitle(event.target.value);
-                                }}/>
-                   </div>
-                   <div class="form-group col-md-6">
-      
-      <Form.Label for="inputEmail4">Email</Form.Label>
-    <Form.Control
-                  type="text"
-                  placeholder="Enter your email"
-                  onChange={(event) => {
-                    return setGameTitle(event.target.value);
-                  }}
-                />
-      
-      </div>
-      </div>
-      <div class="row">
-                   <div class="form-group col-md-6">
-                            <Form.Label for="inputEmail4">Address</Form.Label>
-                              <Form.Control
-                                type="text"  placeholder="Enter your Address"
-                                 onChange={(event) => {
-                                 return setGameTitle(event.target.value);
-                                }}/>
-                   </div>
-                   <div class="form-group col-md-6">
-      
-      <Form.Label for="inputEmail4">Contact Number</Form.Label>
-    <Form.Control
-                  type="text"
-                  placeholder="Enter your contact number"
-                  onChange={(event) => {
-                    return setGameTitle(event.target.value);
-                  }}
-                />
-      
-      </div>
-      </div>
-      <div class="row">
+             <h1>Login</h1>
+           <div class="login">
                    <div class="form-group col-md-6">
                             <Form.Label for="inputEmail4">Username</Form.Label>
                               <Form.Control
-                                type="text"  placeholder="Enter your username"
+                                type="text"  placeholder="Enter your Username"
                                  onChange={(event) => {
-                                 return setGameTitle(event.target.value);
+                                 return setUsername(event.target.value);
                                 }}/>
                    </div>
                    <div class="form-group col-md-6">
@@ -118,29 +73,30 @@ export default function Player_login() {
                   type="text"
                   placeholder="Enter your password"
                   onChange={(event) => {
-                    return setGameTitle(event.target.value);
+                    return setPassword(event.target.value);
                   }}
                 />
       
       </div>
       </div>
+      
+   
            </form>
 
             <button
               className="btn btn-primary border border-success registerbtn"
-              onClick={addEvent}
+              onClick={loginPlayer}
             >
-             Register
+             Login
             </button>
           </div>
         </div>
       )}
       {ShowAlert === true && (
         <div className="alert alert-success" role="alert">
-          <h4 className="alert-heading">Hello Oraganizer. Congratulations!!</h4>
+          <h4 className="alert-heading">Hello Player. Congratulations!!</h4>
           <p>
-            You have Successfully been registered. Please check account
-            section to
+            You have Successfully been logged in.
           </p>
           <hr></hr>
 
