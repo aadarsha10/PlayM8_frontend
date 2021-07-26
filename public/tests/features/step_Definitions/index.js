12b8cc1ad597c822da1970439026526033a65892
dilipbam <57613211+dilipbam@users.noreply.github.com>
2021-07-26 11:59:02 +0545

    changes in test files

--- public/tests/features/step_Definitions/index.js
+++ public/tests/features/step_Definitions/index.js
@@ -1,13 +1,33 @@
-const{ Given } = require("@cucumber/cucumber");
-const { delay } = require("lodash");
+const { expect } = require("chai");
+const { Given } = require("@cucumber/cucumber");
+const { Builder, By, until } = require("selenium-webdriver"); 
+const { delay } = require("../utils/delay");
+const { getDefaultNormalizer } = require("@testing-library/react");
 
-Given("Test registration Functionality", {timeout:3000}, async function (){
+Given("Test registration functionality", {timeout:30000}, async function (){
     let driver = await new Builder().forBrowser("chrome").build();
-    await driver.get("http://localhost:3000/register");
-    await driver.findElement(By.id("firstName")).sendKeys("test");
+    await driver.get("http://localhost:3000/organizer/register");
+    await driver.findElement(By.controlId("fullName")).sendKeys("Aadarsha Man Shrestha");
+    await driver.findElement(By.controlId("contact")).sendKeys("9861763483");
+    await driver.findElement(By.controlId("address")).sendKeys("Kapan");
+    await driver.findElement(By.controlId("email")).sendKeys("aadarsha@gmail.com");
+    await driver.findElement(By.controlId("userName")).sendKeys("aadarsha12");
+    await driver.findElement(By.controlId("password")).sendKeys("Aadarsha123");
     await driver.sleep(delay);
     await driver.findElement(By.id("registration")).click();
 
-    await driver.wait(until.elementLocated(By.id("loginForm")), 30000);
-    expect(await driver.wait(until.elementLocated(By.id("loginForm"))));
+    await driver.wait(until.elementLocated(By.id("Organizer_login")), 30000);
+    expect(await driver.wait(until.elementLocated(By.id("Organizer_login"))));
+});
+
+Given("Test login functionality", {timeout: 30000 }, async function (){
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("http://localhost:3000/organizer/login");
+    await driver.findElement(By.controlId("email")).sendKeys("aadarsha@gmail.com");
+    await driver.findElement(By.controlId("password")).sendKeys("Aadarsha123");
+    await driver.sleep(delay);
+    await driver.findElement(By.id("btnLogin")).click();
+
+    await driver.wait(until.elementLocated(By.id("RegistrationOrganizer")), 30000);
+    expect(await driver.wait(until.elementLocated(By.id("RegistrationOrganizer"))));
 })
