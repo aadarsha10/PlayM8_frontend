bd9d955d0363f6d7e5acf33ff3a1343441fbbd0d
santooshrestha <shrestha.santoo@gmail.com>
2021-07-28 16:20:04 +0545

    admin dashboard with side nav

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,63 +1,60 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-// import NavDropdown from 'react-bootstrap/NavDropdown'
-// import Form from 'react-bootstrap/Form'
-import "../scss/nav.scss";
-// import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
-import Image from 'react-bootstrap/Image'
-import './NavBar.css'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
-import logo from '../images/tabletennis.jpeg'
-import {Link} from 'react-router-dom'
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import "./NavBar.css";
+
+import logo from "../images/tabletennis.jpeg";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
-    return (
-        <Navbar bg="success" expand="lg">
-          
-        {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
-        <a href="/">  <Image src = {logo} alt = "logo" roundedCircle height = "80px" width = "120px"  /></a>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto ">
-          <ul className="navbar-nav ms-auto ">
-          {/* <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2 ml-40x search" />
-          </Form>
-          <Button className="btnsearch">Search</Button> */}
-      
-          <div className="nav">
-          {/* <li className="nav-item active">
-            <Nav.Link className="nav-link" href="/home">Home </Nav.Link>
-          </li> */}
-          {/* <li className="nav-item">
-            <Nav.Link className="nav-link active" href="/addevent">Add Events</Nav.Link>
-          </li>
+  return (
+    <Navbar bg="light" expand="lg">
+      {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+      <Image src={logo} alt="logo" roundedCircle height="200px" width="200px" />
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link className="ml-20x nav-title">PlayM8</Nav.Link>
+        </Nav>
 
-          <li className="nav-item">
-            <Nav.Link className="nav-link active" href="/shop">View Events</Nav.Link>
-          </li>
- */}
+        <Nav className="mr-auto ml-auto">
+          <Nav.Link className="ml-50x nav-sub-title">Home</Nav.Link>
+          <Nav.Link
+            className="ml-20x nav-sub-title"
+            href="/organizer/dashboard/account"
+          >
+            Account
+          </Nav.Link>
+          <Nav.Link
+            className="ml-20x nav-sub-title"
+            href="/organizer/dashboard/addEvents"
+          >
+            Add Events
+          </Nav.Link>
+          <NavDropdown
+            title="Sports Type"
+            id="basic-nav-dropdown"
+            className="ml-20x nav-sub-title"
+          >
+            <NavDropdown.Item href="#sfsdf">Football</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Table Tennis</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Badminton</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Basketball</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Lawn Tennis</NavDropdown.Item>
+            {/* <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">School Ev</NavDropdown.Item> */}
+          </NavDropdown>
+        </Nav>
 
-          <li className="nav-item">
-            <Button variant="light"><Nav.Link className="nav-link active" href="../organizer/register">Register</Nav.Link></Button>
-          </li>
-
-          <li className="nav-item">
-            <Button className="btn btn-primary"><Nav.Link className="nav-link active" href="../organizer/login">Login</Nav.Link></Button>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link active" to="../admin/dashboard">Admin_dashboard</Link>
-          </li>
-          </div>
-
-          </ul>
-          
-    
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    )
+        {/* <Navbar.Brand>Hello Organizer !!!</Navbar.Brand> */}
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
