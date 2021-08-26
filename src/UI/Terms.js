98fbbddec20db27da43a6625830879748004605b
Dejina Ghale <ghaledejina@gmail.com>
2021-08-26 13:07:07 +0545

    terms and conditions

---
+++ src/UI/Terms.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "../../Components/Footer";
+import LandingNavbar from "../../Components/LandingNavbar";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+class About extends Component {
+    render() {
+      return (
+        <div>
+          <LandingNavbar />
+          <div>
+          <h1>Terms Of Service</h1>
+          By signing up for the Playpass services or any of the services of Playpass Inc. ("Playpass") you are agreeing to be bound by the following terms and conditions ("Terms of Service"). The services offered by Playpass under the Terms of Service include various products and services to help you organize recreational sports and activities for players. Any such services offered by Playpass are referred to in these Terms of Services as the "Services". Any new features or tools which are added to the current Services shall be also subject to the Terms of Service. You can review the current version of the Terms of Service at any time at https://playpass.com/terms. Playpass reserves the right to update and change the Terms of Service by posting updates and changes to the Playpass website. You are advised to check the Terms of Service from time to time for any updates or changes that may impact you.
+<br/>
+You must read, agree with and accept all of the terms and conditions contained in this Terms of Service agreement, including the Playpass Privacy Policy, Acceptable Use Policy, and Data Processing Addendum, before you may become a Playpass user.
+          <br/> <br/>
+          <h1>Account Terms</h1>
+          You must be 18 years or older or at least the age of majority in the jurisdiction where you reside or from which you use this Service.
+To access and use the Services, you must register for a Playpass account ("Account") by providing your full legal name, current address, phone number, a valid email address, and any other information indicated as required. Playpass may reject your application for an Account, or cancel an existing Account, for any reason, in our sole discretion.
+You acknowledge that Playpass will use the email address you provide as the primary method for communication.
+You are responsible for keeping your password secure. Playpass cannot and will not be liable for any loss or damage from your failure to maintain the security of your Account and password.
+You are responsible for all activity and content such as photos, images, videos, graphics, written content, audio files, code, information, or data uploaded, collected, generated, stored, displayed, distributed, transmitted or exhibited on or in connection with your Account ("Materials").
+A breach or violation of any term in the Terms of Service, as determined in the sole discretion of Playpass will result in an immediate termination of your services.
+<br/><br/>
+<h1>General Conditions</h1>
+You must read, agree with and accept all of the terms and conditions contained in these Terms of Service, including the Privacy Policy before you may become a member of Playpass.
+
+Technical support is only provided to paying Account holders and is only available via email, chat, or phone depending on your billing plan.
+The Terms of Service shall be governed by and interpreted in accordance with the laws of the State of California and the laws of the United States applicable therein, without regard to principles of conflicts of laws. The parties irrevocably and unconditionally submit to the exclusive jurisdiction of the courts of the State of California with respect to any dispute or claim arising out of or in connection with the Terms of Service. The United Nations Convention on Contracts for the International Sale of Goods will not apply to these Terms of Service and is hereby expressly excluded.
+You acknowledge and agree that Playpass may amend these Terms of Service at any time by posting the relevant amended and restated Terms of Service on the Playpass website, available at https://playpass.com/terms and such amendments to the Terms of Service are effective as of the date of posting. Your continued use of the Services after the amended Terms of Service are posted to the Playpass website constitutes your agreement to, and acceptance of, the amended Terms of Service. If you do not agree to any changes to the Terms of Service, do not continue to use the Service.
+You may not use the Playpass service for any illegal or unauthorized purpose nor may you, in the use of the Service, violate any laws in your jurisdiction (including but not limited to copyright laws), the laws applicable to you in your customer's jurisdiction, or the laws of the United States and the State of California. You will comply with all applicable laws, rules, and regulations in your use of the Service.<br/>
+You agree not to reproduce, duplicate, copy, sell, resell or exploit any portion of the Service, use of the Service, or access to the Service without the express written permission by Playpass.
+You shall not purchase search engine or other pay per click keywords (such as Google AdWords), or domain names that use Playpass or Playpass trademarks and/or variations and misspellings thereof.
+Questions about the Terms of Service should be sent to support@playpass.com.
+You acknowledge and agree that your use of the Service, including information transmitted to or stored by Playpass, is governed by its Privacy Policy at https://playpass.com/privacy<br/>
+<br/>
+<h1>PlayPass Rights</h1>
+We reserve the right to modify or terminate the Service for any reason, without notice at any time.
+We reserve the right to refuse service to anyone for any reason at any time.
+We may, but have no obligation to, remove Materials and suspend or terminate Accounts if we determine in our sole discretion that the services or goods offered via an organizer's site, or the Materials uploaded or posted to an organizer's site, violate our these Terms of Service.
+Verbal or written abuse of any kind (including threats of abuse or retribution) of any Playpass customer, Playpass employee, member, or officer will result in immediate Account termination.
+Playpass does not pre-screen Materials and it is in our sole discretion to refuse or remove any Materials from the Service.
+We reserve the right to provide our services to your competitors and make no promise of exclusivity in any particular market segment.
+In the event of a dispute regarding Account ownership, we reserve the right to request documentation to determine or confirm Account ownership. Documentation may include, but is not limited to, a scanned copy of your business license, government-issued photo ID, the last four digits of the credit card on file, etc.
+Playpass retains the right to determine, in our sole judgment, rightful Account ownership and transfer an Account to the rightful owner. If we are unable to reasonably determine the rightful Account owner, Playpass reserves the right to temporarily disable an Account until a resolution has been determined by the disputing parties<br/>
+<br/>
+<h1> Privacy and data protection</h1>
+Playpass is firmly committed to protecting the privacy of your personal information and the personal information of your customers. By using the Service, you acknowledge and agree that Playpass' collection, usage, and disclosure of this personal information is governed by our Privacy Policy.
+
+Additionally, if: (a) you are established in the European Economic Area (EEA); (b) you provide goods or services to customers in the EEA; or (c) you are otherwise subject to the requirements of the EU General Data Protection Regulation, Playpass' collection and use of personal information of any European residents is also subject to our Data Processing Addendum.
+          </div>
+         
+            <Footer />
+        </div>
+      );
+    }
+  }
+  export default About;
+  
